bf5529876570b864f5fb19e10638918e5263aadd
Gustavo Rodrigues <gustavoaft78@outlook.com>
2026-07-10 19:12:12 -0300

    feat: adiciona modelo Pydantic para tarefa e usuario

---
+++ backend/schemas/tarefa_schema.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel,EmailStr
+
+class tarefaSchema(BaseModel):
+  nome_tarefa : str
+  descricao_tarefa : str
+  status_tarefa : str
+
+
+class usuarioSchema(BaseModel):
+  nome_usuario : str
+  email : EmailStr
+  senha_hash : str
