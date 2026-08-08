1576925099039d433b16e728d4e7d8fda05e4659
Gustavo Rodrigues <gustavoaft78@outlook.com>
2026-08-07 23:25:06 -0300

    refactor: muda nome de rotas de  usuario

--- server/server.py
+++ server/server.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 app = FastAPI(title= "FastAPI - To do list")
 
-
 from backend.routes import tarefas_routes
 from backend.routes import user_routes
 
 # renderizando as rotas
 app.include_router(tarefas_routes.router_tarefas)
-app.include_router(user_routes.user_routes)
+app.include_router(user_routes.router_user)
 
