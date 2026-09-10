484b30ceda025f52450677a56d5d64730c16e8f9
Gustavo Rodrigues <gustavoaft78@outlook.com>
2026-09-10 00:45:27 -0300

    fix: corrigi importação de modulo e estrutura de pastas

--- apps/backend/database/conexão_banco.py
+++ apps/backend/database/conexão_banco.py
@@ -2,12 +2,22 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+print("1 - iniciou")
+
 # carrega o .env
 load_dotenv()
 
+print("2 - carregou .env")
+
+print("USER:", os.getenv("DB_USER"))
+print("HOST:", os.getenv("DB_HOST"))
+print("PORT:", os.getenv("DB_PORT"))
+print("NAME:", os.getenv("DB_NAME"))
+
 def conecta_banco():
   
-    
+  print("3 - entrou na função")
+
   # Faz a montagem da engine
   engine = create_engine(
     f"postgresql+psycopg2://{os.getenv('DB_USER')}:"
@@ -17,6 +27,8 @@ def conecta_banco():
     f"{os.getenv('DB_NAME')}",
     pool_pre_ping=True
   )
+  
+  print("4 - criou engine")
 
   # Teste da montagem da engine e conexão ao banco
   try:
@@ -28,3 +40,5 @@ def conecta_banco():
     print("Erro ao conectar no banco ❌")
     print(f"Erro: {e}")
 
+
+conecta_banco()
--- backend/models/usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario():
-  def __init__(self,nome_usuario ,email, senha, id = None ) :
-    self.id = id
-    self.nome_usuario = nome_usuario
-    self.email = email
-    self.senha = senha
-    
-    
-    
