8343ff590e6fc9202c1a2fab52044511ab704ade
Gustavo Rodrigues <gustavoaft78@outlook.com>
2026-09-11 00:31:55 -0300

    chore: remove print e logica desnecessaria

--- apps/backend/database/conexão_banco.py
+++ apps/backend/database/conexão_banco.py
@@ -2,21 +2,12 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
-print("1 - iniciou")
-
 # carrega o .env
 load_dotenv()
 
-print("2 - carregou .env")
-
-print("USER:", os.getenv("DB_USER"))
-print("HOST:", os.getenv("DB_HOST"))
-print("PORT:", os.getenv("DB_PORT"))
-print("NAME:", os.getenv("DB_NAME"))
 
 def conecta_banco():
   
-  print("3 - entrou na função")
 
   # Faz a montagem da engine
   engine = create_engine(
@@ -27,8 +18,6 @@ def conecta_banco():
     f"{os.getenv('DB_NAME')}",
     pool_pre_ping=True
   )
-  
-  print("4 - criou engine")
 
   # Teste da montagem da engine e conexão ao banco
   try:
@@ -40,5 +29,3 @@ def conecta_banco():
     print("Erro ao conectar no banco ❌")
     print(f"Erro: {e}")
 
-
-conecta_banco()
--- apps/backend/models/tarefa.py
+++ apps/backend/models/tarefa.py
@@ -1,12 +1,9 @@
 class Tarefa():
     
-  def __init__(self,tarefa,descricao_tarefa,status_tarefa,id=None):
-    self.id = id
-    self.nome_tarefa = tarefa
+  def __init__(self,nome_tarefa,descricao_tarefa,status_tarefa,id_usuario=None):
+    self.id_usuario = id_usuario
+    self.nome_tarefa = nome_tarefa
     self.descricao_tarefa = descricao_tarefa
     self.status_tarefa = status_tarefa
 
-# Verifica se não possui numeros digitados no input
-    if tarefa.isdigit():
-        raise ValueError("A tarefa deve conter apenas letras")
 
